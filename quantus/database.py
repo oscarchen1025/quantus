e4ffe241bb309bcdcf6d64412da1ffe37198660b
oscar <oscarchen.1025@gmail.com>
2025-12-23 15:38:28 +0800

    add plot.py

--- quantus/database.py
+++ quantus/database.py
@@ -28,3 +28,9 @@ class DataBase:
 
         return pd.read_pickle(f"{self.path}/{file_name}.pickle")
 
+    def get_klines(self,stock_id):
+
+        ohlcv = pd.DataFrame({name:self.get(fname)[stock_id] for name,fname in [('close','收盤價'),('open','開盤價'),('high','最高價'),('low','最低價'),('volume','成交股數')]})
+
+        return ohlcv
+
---
+++ quantus/plot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def plot_klines(ohlcv,end_date=None,n_bars=180):
+
+    s = ohlcv.copy().loc[:end_date or pd.to_datetime('now')].tail(n_bars)
+
+    fig = go.Figure(data=[go.Candlestick(x=s.index,open=s['open'],high=s['high'],low=s['low'],close=s['close'],increasing_line_color='#ba4545',decreasing_line_color='#27847c')])
+
+    fig.update_xaxes(rangebreaks=[dict(values=s.reindex(pd.date_range(s.index[0],s.index[-1])).pipe(lambda s:s[s['close'].isna()]).index)])
+    fig.update_layout(width=1300,height=500,template='plotly_dark',plot_bgcolor='#1a1b20',paper_bgcolor='#1a1b20')
+    fig.show()
