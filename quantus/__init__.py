52dd884eaff59053755dc9263bd849d32539d1dd
oscar <oscarchen.1025@gmail.com>
2025-12-26 12:13:25 +0800

    add get_snapshot_data

--- quantus/__init__.py
+++ quantus/__init__.py
@@ -2,7 +2,7 @@ from IPython import get_ipython
 import os
 
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 COLAB_ENV = 'google.colab' in str(get_ipython())
 CONNECTED = os.path.exists('/content/drive/MyDrive')
--- quantus/account.py
+++ quantus/account.py
@@ -1,4 +1,10 @@
 import shioaji as sj
+import pandas as pd
+
+from quantus.database import DataBase
+
+
+db = DataBase()
 
 
 class Account:
@@ -28,3 +34,16 @@ class Account:
             trade = self.api.place_order(contract=contract,order=order)
 
             print('Auth success !')
+
+    def get_snapshot_data(self,stock_ids:list=None):
+
+        stock_ids = stock_ids or db.get('收盤價',exclude_etf=True).tail(200).dropna(how='all',axis=1).columns
+
+        contracts = [self.api.Contracts.Stocks[stock_id] for stock_id in stock_ids]
+        snapshots = self.api.snapshots([contract for contract in contracts if contract is not None])
+
+        df = pd.DataFrame(s.__dict__ for s in snapshots)
+        df['ts'] = pd.to_datetime(df['ts'])
+        df = df.rename(columns={'code':'證券代號'})
+
+        return df
