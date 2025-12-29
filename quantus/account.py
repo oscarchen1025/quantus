2d6c69dbe9e50860cff5b9d0ed15ae5cab80a615
oscar <oscarchen.1025@gmail.com>
2025-12-29 12:22:44 +0800

    revise

--- quantus/account.py
+++ quantus/account.py
@@ -83,8 +83,8 @@ class Account:
 
         display(
             df.style
-            .bar(color=['#049981c3','#cd323fc3'],width=100,height=20,align='zero',subset=['change_rate','kbar','1week','1month','1year','ytd'])
+            .bar(color=['#049981c3','#cd323fc3'],width=100,height=20,align='zero',subset=['change_rate','kbar_rate','1week','1month','1year','ytd'])
             .bar(color=['#049981','#3b8ae8'],width=50,height=25,align='zero',subset=['volume_ratio'])
-            .format(lambda s:str(round(s,2)) + ' %',subset=['change_rate','kbar','volume_ratio','1week','1month','1year','ytd'])
+            .format(lambda s:str(round(s,2)) + ' %',subset=['change_rate','kbar_rate','volume_ratio','1week','1month','1year','ytd'])
             .format(lambda s:s,subset=['close','total_volume'])
         )
